caa79a6b68bc84ed2eb522530c4e6a0890f906fe
cola777jz <cola777jz@gmail.com>
2023-09-24 16:24:39 +0800

    Deploy SpringBoot Project With IDEA + Docker

--- DeployWithDocker/src/main/java/jz/cola/deploywithdocker/DeployWithDockerApplication.java
+++ DeployWithDocker/src/main/java/jz/cola/deploywithdocker/DeployWithDockerApplication.java
@@ -1,13 +1,38 @@
 package jz.cola.deploywithdocker;
 
+import jakarta.websocket.server.PathParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Slf4j
 public class DeployWithDockerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DeployWithDockerApplication.class, args);
+
+        log.info("DeployWithDockerApplication run successful ");
     }
+
+    @RestController
+    @RequestMapping("api/v1/hello")
+    static class HelloController{
+        @GetMapping("/sayHello/{name}")
+        public String sayHello(@PathVariable("name") String name){
+            return "Hello I am " + name;
+        }
+
+        @GetMapping("/start")
+        public String start(){
+            return "YuanShen QiDong!";
+        }
+    }
+
+
 
 }
